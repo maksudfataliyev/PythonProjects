83eaa67c7882644c4bac0e29d23cce47c8b9cf8b
geniymaksud <143101499+maksudfataliyev@users.noreply.github.com>
2025-01-09 19:35:47 +0400

    Add files via upload

---
+++ String_practice.py
@@ -0,0 +1,127 @@
+import random
+
+labirint = ("######\n"
+            "#S##E#\n"
+            "# ## #\n"
+            "# #  #\n"
+            "#   ##\n"
+            "######")
+
+print(labirint)
+while True:
+    space1 = random.randint(11,13)
+    if space1 == 13:
+        print("######\n"
+              "# ##E#\n"
+              "#S## #\n"
+              "# #  #\n"
+              "#   ##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
+while True:
+    space2 = random.randint(17,19)
+    if space2 == 19:
+        print("######\n"
+              "# ##E#\n"
+              "# ## #\n"
+              "#S#  #\n"
+              "#   ##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
+while True:
+    space3 = random.randint(23,25)
+    if space3 == 25:
+        print("######\n"
+              "# ##E#\n"
+              "# ## #\n"
+              "# #  #\n"
+              "#S  ##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
+while True:
+    space4 = random.randint(24,26)
+    if space4 == 26:
+        print("######\n"
+              "# ##E#\n"
+              "# ## #\n"
+              "# #  #\n"
+              "# S ##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
+while True:
+    space5 = random.randint(25,27)
+    if space5 == 27:
+        print("######\n"
+              "# ##E#\n"
+              "# ## #\n"
+              "# #  #\n"
+              "#  S##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
+while True:
+    space6 = random.randint(19,21)
+    if space6 == 21:
+        print("######\n"
+              "# ##E#\n"
+              "# ## #\n"
+              "# #S #\n"
+              "#   ##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
+while True:
+    space7 = random.randint(20,22)
+    if space7 == 22:
+        print("######\n"
+              "# ##E#\n"
+              "# ## #\n"
+              "# # S#\n"
+              "#   ##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
+while True:
+    space8 = random.randint(14,16)
+    if space8 == 16:
+        print("######\n"
+              "# ##E#\n"
+              "# ##S#\n"
+              "# #  #\n"
+              "#   ##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
+while True:
+    end = random.randint(8,10)
+    if end == 10:
+        print("######\n"
+              "# ##S#\n"
+              "# ## #\n"
+              "# #  #\n"
+              "#   ##\n"
+              "######")
+        break
+    else:
+        print("Ищу путь")
+        continue
